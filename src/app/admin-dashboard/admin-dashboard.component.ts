e75c804fc2c489beb5f6280255e8bdf1b6b61931
RashiniMaduka <rashinimaduka1029@gmail.com>
2020-07-01 23:41:35 +0530

    'dashboardupdated'

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -3,6 +3,21 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
+export interface PeriodicElement {
+  Agent: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1,  Agent: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2,  Agent: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, Agent: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4,  Agent: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+   
+   
+];
  
 
 @Component({
@@ -11,6 +26,8 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
    
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
   .pipe(
