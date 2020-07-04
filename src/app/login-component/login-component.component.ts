e652881ac8105c7935b5f36501d56016ff4d4b4e
Akitha Nethmal <akithanethmal@gmail.com>
2020-07-04 15:30:09 +0530

    login ts

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../models/login.model';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
-  styleUrls: ['./login-component.component.css']
+  styleUrls: ['./login-component.component.css'],
 })
 export class LoginComponentComponent implements OnInit {
+  login: Login = new Login();
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({ email: [''], password: [''] });
   }
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    const login = {
+      email: this.email.value,
+      password: this.password.value,
+    };
+    console.log(login);
+  }
 }
