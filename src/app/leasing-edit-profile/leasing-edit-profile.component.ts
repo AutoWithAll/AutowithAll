651c8a0bb609d57ac45404e03b58187e0a6ced68
asangika <asangikasandamini@gmail.com>
2020-07-08 01:30:25 -0700

    edit-proile modified

--- src/app/leasing-edit-profile/leasing-edit-profile.component.ts
+++ src/app/leasing-edit-profile/leasing-edit-profile.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-leasing-edit-profile',
   templateUrl: './leasing-edit-profile.component.html',
   styleUrls: ['./leasing-edit-profile.component.css']
 })
-export class LeasingEditProfileComponent implements OnInit {
+export class LeasingEditProfileComponent {
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+   
 }
