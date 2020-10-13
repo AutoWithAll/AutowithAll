83c539247844c8b01d565415d683f7667abb7416
Prabath15969 <prabathmadhu1996@gmail.com>
2020-10-13 22:19:12 +0530

    123

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 import {UserService  } from "../service/user.service";
 import { HomeService } from '../service/home.service';
-import { User } from '../models/user.model';
 
 // import { MaterialModule } from '@angular/material'
 
@@ -14,11 +13,11 @@ import { User } from '../models/user.model';
 })
 export class HomeComponent implements OnInit {
   list : any;
-  agentList : any = <any>{};
+  agentList;
   img;
   showNavigationArrows = false;
   showNavigationIndicators = false;
-  images = [1071, 1072, 111].map((n) => `https://picsum.photos/id/${n}/400/200`);
+  images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   constructor(config: NgbCarouselConfig, private service: UserService , private homeService : HomeService) {
     // customize default values of carousels used by this component tree
@@ -29,8 +28,11 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.getAgentList();
     this.service.getads().subscribe(res=>{
-      console.log("home geta ads" + res);
+      console.log(res);
       this.list = res;
+    },
+    err => {
+      console.log(err)
     })
 
   }
